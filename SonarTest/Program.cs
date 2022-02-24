cc843e8a92de4ddb7d71d7cf97d7c5922776355c
Nathan Santanna <nathan.santanna@bd.com>
2022-02-24 18:09:14 -0300

    test

--- SonarTest/Program.cs
+++ SonarTest/Program.cs
@@ -6,7 +6,7 @@ namespace SonarTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!");
         }
     }
 }
